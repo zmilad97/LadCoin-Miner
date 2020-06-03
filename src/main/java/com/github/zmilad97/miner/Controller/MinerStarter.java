30a3ceac5bf3865e9c6da3fb531859c9e965dc50
isa <esa.hekmat@gmail.com>
2020-06-03 19:54:24 +0430

    set log level in property file

--- src/main/java/com/github/zmilad97/miner/Controller/MinerStarter.java
+++ src/main/java/com/github/zmilad97/miner/Controller/MinerStarter.java
@@ -2,6 +2,8 @@ package com.github.zmilad97.miner.Controller;
 
 import com.github.zmilad97.miner.Module.Block;
 import com.github.zmilad97.miner.Service.MinerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -9,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MinerStarter implements ApplicationRunner {
-
+  private static final Logger LOG = LoggerFactory.getLogger(MinerStarter.class);
   private final MinerService minerService;
 
   @Autowired
@@ -19,6 +21,7 @@ public class MinerStarter implements ApplicationRunner {
 
   @Override
   public void run(ApplicationArguments args) {
+    LOG.debug("application just started, start to mine...");
     Block block = minerService.findBlock();
     minerService.computeHash(block);
     minerService.sendBlock(block);
