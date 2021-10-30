e86ff424539253686f11a421bdbea49d4fa1421d
milad <zmilad97@gmail.com>
2021-10-30 17:04:26 +0330

    cleanup with lombok

--- src/main/java/com/github/zmilad97/miner/Module/Block.java
+++ src/main/java/com/github/zmilad97/miner/Module/Block.java
@@ -1,18 +1,38 @@
 package com.github.zmilad97.miner.Module;
 
 import com.github.zmilad97.miner.Module.Transaction.Transaction;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@NoArgsConstructor
 public class Block {
 
+    @Getter
+    @Setter
     private int index;
+    @Getter
+    @Setter
     private String date;
+    @Getter
+    @Setter
     private String hash;
+    @Getter
+    @Setter
     private String previousHash;
+    @Getter
+    @Setter
     private long nonce;
+    @Getter
+    @Setter
     private List<Transaction> transactions;
+    @Getter
+    @Setter
     private String difficultyLevel;
+    @Getter
+    @Setter
     private Double reward;
 
 
@@ -20,105 +40,9 @@ public class Block {
         this.index = index;
         this.date = date;
         this.transactions = transactions;
-
-    }
-
-    public Block() {
-    }
-
-
-
-//    public String computeHash(String condition) {
-//
-//        String hash = "null";
-//        nonce = -1;
-//
-//        try {
-//            do {
-//                nonce++;
-//                String stringtohash = nonce + this.index + this.date.toString() + this.previousHash + this.transactions;
-//
-//                Cryptography cryptography = new Cryptography();
-//                hash = cryptography.toHexString(cryptography.getSha(stringtohash));
-//
-//                if (hash.startsWith(condition))
-//                    break;
-//
-//            } while (true);
-//
-//        } catch (NoSuchAlgorithmException e) {
-//            e.printStackTrace();
-//        }
-//        setNonce(nonce);
-//        return hash;
-//    }
-
-
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getHash() {
-        return hash;
-    }
-
-    public void setHash(String hash) {
-        this.hash = hash;
-    }
-
-    public String getPreviousHash() {
-        return previousHash;
-    }
-
-    public void setPreviousHash(String previousHash) {
-        this.previousHash = previousHash;
-    }
-
-    public List<Transaction> getTransactions() {
-        return transactions;
-    }
-
-    public void setTransactions(List<Transaction> transactions) {
-        this.transactions = transactions;
-    }
-
-    public long getNonce() {
-        return nonce;
-    }
-
-    public void setNonce(long nonce) {
-        this.nonce = nonce;
-    }
-
-    public String getDifficultyLevel() {
-        return difficultyLevel;
-    }
-
-    public void setDifficultyLevel(String difficultyLevel) {
-        this.difficultyLevel = difficultyLevel;
-    }
-
-    public Double getReward() {
-        return reward;
-    }
-
-    public void setReward(Double reward) {
-        this.reward = reward;
     }
 
     public void addTransaction(Transaction transaction) {
-        transactions.add(transaction);
+        this.transactions.add(transaction);
     }
 }
